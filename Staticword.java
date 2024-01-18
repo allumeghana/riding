b00a96e3af32c19b5040c71ab2b696210a1fb9f7
allumeghana <103098782+allumeghana@users.noreply.github.com>
2024-01-18 16:56:29 +0530

    Add files via upload

---
+++ Staticword.java
@@ -0,0 +1,12 @@
+package staticword;
+
+public class Staticword {
+	static void hi(String n) { 
+	System.out.println ("hello");{
+	}
+	}
+	public static void main(String[] args) {
+		hi(null);
+	}
+
+}
